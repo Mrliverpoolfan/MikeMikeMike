af71e392fad0686d3a5b863b21633ac1ade1a6da
Harrison Statham <harrison.statham@comcast.net>
2017-07-13 08:03:16 -0700

    Update PhaseAttr.cc

--- PhaseAttr.cc
+++ PhaseAttr.cc
@@ -1,38 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 
+// TODO: Include any headers here that are needed.
+
 using namespace std;
 
-
-class PhaseAttributes
-{
-	public:
-		PhaseAttributes(float, float); //Sets permittivity and volumetricFraction
-		PhaseAttributes(); //Set permittivity and volumetricFraction to initial values of 1
-		
-		// You will need to define the getters and setters for this class.
-		//Make the setter functions so that they can be cascaded.
-
-		// Getters
-		float getPermittivity() const;
-		float getVolumetricFraction() const;
-
-		// Setters
-		//
-		// By returning a reference to the class itself, we can chain methods like so:
-		// PhaseAttributes pa();
-		// pa.setPermittivity(1.0).setVolumetricFraction(1.0);
-		//
-		PhaseAttributes & setPermittivity(float);
-		PhaseAttributes & setVolumetricFraction(float);
-
-	private:
-		float permittivity;
-		float volumetricFraction;
-};
-
-
 // Constructor
+// 
+//
+PhaseAttributes::PhaseAttributes()
+{
+	this->permittivity = 1.0;
+	this->volumetricFraction = 1.0;
+}
+
+
+// Constructor (Overloaded)
 // 
 //
 PhaseAttributes::PhaseAttributes(float permittivity, float volumetricFraction)
